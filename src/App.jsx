a3af1a8650758176e20ba4032589f63377199d66
Razzak118348 <abdurrazzak118348@gmail.com>
2026-03-08 22:12:28 +0600

    project setup and add navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Navbar from './Component/Navbar/Navbar'
+
+function App() {
+
+  return (
+  <>
+  <Navbar></Navbar>
+<div className='text-black'>this is app </div>
+  </>
+  )
+}
+
+export default App
